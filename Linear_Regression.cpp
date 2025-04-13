6c97b108c4c71be838af9cb4ec3b4e88468d5a30
kss418 <jis1595@naver.com>
2025-04-13 14:01:06 +0900

    행렬 연산 함수 추가

--- Linear_Regression.cpp
+++ Linear_Regression.cpp
@@ -4,26 +4,54 @@
 class Data{
 public:
     std::vector<std::vector<float>> v;
-    Data Transpose(){
-        int y = v.size(), x = v[0].size();
-        Data ret; ret.v.resize(x, std::vector<float>(y));
+    int y = 0, x = 0;
 
+    Data(){}
+    Data(int y, int x){ 
+        v.resize(y, std::vector<float>(x)); 
+        this->y = y; this->x = x;
+    }
+
+    Data Transpose(){
+        Data ret(y, x);
         for(int i = 0;i < x;i++){
             for(int j = 0;j < y;j++) ret.v[i][j] = v[j][i];
         }
 
         return ret;
     }
+
+    static Data Ones(int y, int x){
+        Data ret(y, x);
+        for(auto& i : ret.v){
+            for(auto& j : i) j = 1;
+        }
+        return ret;
+    }
+
+    static Data Zeros(int y, int x){
+        Data ret(y, x);
+        for(auto& i : ret.v){
+            for(auto& j : i) j = 1;
+        }
+        return ret;
+    }
+
+    static Data Concatenate(Data& a, Data& b){
+        Data ret(a.y, a.x + b.x);
+        for(int i = 0;i < a.y;i++){
+            for(int j = 0;j < a.x;j++) ret.v[i][j] = a.v[i][j];
+            for(int j = 0;j < b.x;j++) ret.v[i][j + a.x] = b.v[i][j];
+        }
+        return ret;
+    }
 };
 
 Data operator * (const Data& a, const Data& b){
-    int ay = a.v.size(), ax = a.v[0].size();
-    int by = b.v.size(), bx = b.v[0].size();
-
-    Data ret; ret.v.resize(ay, std::vector<float>(bx));
-    for(int i = 0;i < ay;i++){
-        for(int j = 0;j < bx;j++){
-            for(int k = 0;k < ax;k++) ret.v[i][j] += a.v[i][k] * b.v[k][j];
+    Data ret(a.y, b.x);
+    for(int i = 0;i < a.y;i++){
+        for(int j = 0;j < b.x;j++){
+            for(int k = 0;k < a.x;k++) ret.v[i][j] += a.v[i][k] * b.v[k][j];
         }
     }
 
@@ -32,10 +60,13 @@ Data operator * (const Data& a, const Data& b){
  
 
 class Linear_Regression{
-    public:
+public:
+    Data weight;
 
+    Linear_Regression(){
+        weight.v.resize(5, std::vector<float>(5));
+    }
 
-   
     float F(float w, float x){
         return w * x;
     }
@@ -45,13 +76,13 @@ class Linear_Regression{
         return ret * ret;
     }
 
-    float Get_Error(Data& data, Data& w){
+    float Get_Error(Data& data, Data& weight){
 
     }
 
-    void Fit(Data& data, Data& w, int loop = 1000){
+    void Fit(Data& data, int loop = 1000){
         while(loop--){
-            float error = Get_Error(data, w);
+            float error = Get_Error(data, weight);
             
         }
 
@@ -60,11 +91,11 @@ class Linear_Regression{
 
 
 int main(){
-    Data a, b, c;
+    Data a(2, 2), b(2, 2);
     a.v = {{5, 3}, {2, 4}};
     b.v = {{2, 3}, {3, 4}};
 
-    c = a.Transpose();
+    Data c = Data::Concatenate(a, b);
     for(auto& i : c.v){
         for(auto& j : i) std::cout << j << " ";
         std::cout << "\n";
