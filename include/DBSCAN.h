38c0f18a6a9983caf7e5ed2648740b06e7c5cfd2
kss418 <jis1595@naver.com>
2025-04-20 23:17:59 +0900

    Distance 함수 추가

--- include/DBSCAN.h
+++ include/DBSCAN.h
@@ -4,9 +4,13 @@ class DBSCAN{
 public:
     DBSCAN(int min_count, float eps);
     float Get_Error(const Data& x);
+    float Distance(const Data& a, const Data& b);
     void Fit(const Data& x);
 private:
     std::vector<Data> centroids;
     std::vector<int> labels;
     std::vector<std::vector<Data>> clusters;
+    
+    int min_count;
+    float eps;
 };
---
+++ src/DBSCAN.cpp
@@ -0,0 +1,10 @@
+#include "../include/DBSCAN.h"
+
+DBSCAN::DBSCAN(int min_count, float eps) : min_count(min_count), eps(eps) {}
+
+float DBSCAN::Distance(const Data& a, const Data& b){
+    float ret = 0;
+    for(size_t i = 0;i < a.x;i++){
+        
+    }
+}
