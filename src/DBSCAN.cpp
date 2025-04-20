f4a3ca825495da205e6eb6dde50c72eeadd5d233
kss418 <jis1595@naver.com>
2025-04-20 23:18:25 +0900

    Distance 함수 추가

--- src/DBSCAN.cpp
+++ src/DBSCAN.cpp
@@ -5,6 +5,8 @@ DBSCAN::DBSCAN(int min_count, float eps) : min_count(min_count), eps(eps) {}
 float DBSCAN::Distance(const Data& a, const Data& b){
     float ret = 0;
     for(size_t i = 0;i < a.x;i++){
-        
+        float diff = a.v[0][i] - b.v[0][i];
+        ret += diff * diff;
     }
+    return ret;
 }
